cf64b636ce17efbe9dab823cab8f9a453974104c
bilel <bilelh532@gmail.com>
2019-05-03 14:34:22 +0200

    Creation de la liste de pizza dans le Main

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -2,10 +2,30 @@ package fr.pizzeria.console;
 
 import java.util.Scanner;
 
+import fr.pizzeria.model.Pizza;
+
 public class PizzeriaAdminConsoleApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Pizza a = new Pizza (0 ,"PEP" , "pépéroni" , 12.50) ;
+		Pizza b = new Pizza (1 ,"MAR" , "Margherita" , 14.00) ;
+		Pizza c = new Pizza (2 ,"REIN" , "La Reine" , 11.50) ;
+		Pizza d = new Pizza (3, "FRC" , "La 4 Fromage" , 12.00) ;
+		Pizza e = new Pizza (4 ,"CAN" , "La Cannibale" , 12.50) ;
+		Pizza f = new Pizza (5 ,"SAV" , "La Savoyarde" , 13.00) ;
+		Pizza g = new Pizza (6 ,"ORI" , "L'Orientale" , 13.50) ;
+		Pizza h = new Pizza (7 ,"IND" , "L'Indienne" , 14.00) ;
+		System.out.println(a.code + " -> " + a.libelle + " ( " + a.prix + " )");
+		System.out.println(b.code + " -> " + b.libelle + " ( " + b.prix + " )");
+		System.out.println(c.code + " -> " + c.libelle + " ( " + c.prix + " )");
+		System.out.println(d.code + " -> " + d.libelle + " ( " + d.prix + " )");
+		System.out.println(e.code + " -> " + e.libelle + " ( " + e.prix + " )");
+		System.out.println(f.code + " -> " + f.libelle + " ( " + f.prix + " )");
+		System.out.println(g.code + " -> " + g.libelle + " ( " + g.prix + " )");
+		System.out.println(h.code + " -> " + h.libelle + " ( " + h.prix + " )");
+		
 		
 		Scanner questionUser = new Scanner(System.in);
 		//int nombre_1 = questionUser.nextInt();

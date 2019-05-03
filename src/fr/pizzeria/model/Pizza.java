705b1e647106333847014666a0cf19c16a8d1616
bilel <bilelh532@gmail.com>
2019-05-03 12:37:51 +0200

    Creation du 1er constructeur dans la class Pizza

--- src/fr/pizzeria/model/Pizza.java
+++ src/fr/pizzeria/model/Pizza.java
@@ -6,5 +6,11 @@ public class Pizza {
 	String code ;
 	String libelle ;
 	double prix ;
+	
+	public Pizza (String code , String libelle , double prix) {
+		this.code = code ;
+		this.libelle = libelle ;
+		this.prix = prix ;
+	}
 
 }

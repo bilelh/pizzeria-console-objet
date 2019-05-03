8ef866bd234a4b8af114eac5d2c540e75b15049b
bilel <bilelh532@gmail.com>
2019-05-03 12:22:28 +0200

    choix de l'utilisateur fonctionne

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -20,20 +20,33 @@ public class PizzeriaAdminConsoleApp {
 		System.out.println("Veuillez faire votre choix");
 		int user_choice = questionUser.nextInt();
 		
-		if (user_choice == 1) {
-			System.out.println("Liste des pizzas  ");
-		} else if ( user_choice == 2) {
-			System.out.println("Ajout d'une nouvelle pizza  ");
-		} else if ( user_choice == 3) {
-			System.out.println("Mise à jour d'une pizza  ");
-		} else if ( user_choice == 4) {
-			System.out.println("Suppression d'une pizza ");
-		} else if ( user_choice == 99) {
-			System.out.println("Aurevoir :(  ");
+		while (user_choice != 99) {
+		
+		
+			if (user_choice == 1) {
+				System.out.println("Liste des pizzas  ");
+			} else if ( user_choice == 2) {
+				System.out.println("Ajout d'une nouvelle pizza  ");
+			} else if ( user_choice == 3) {
+				System.out.println("Mise à jour d'une pizza  ");
+			} else if ( user_choice == 4) {
+				System.out.println("Suppression d'une pizza ");
+			} 
+			
+			System.out.println("***** Pizzeria Administration *****  ");
+			System.out.println("1. Lister les pizzas  ");
+			System.out.println("2. Ajouter une nouvelle pizza  ");
+			System.out.println("3. Mettre à jour une pizza  ");
+			System.out.println("4. Supprimer une pizza  ");
+			System.out.println("99. Sortir  ");
+			
+			System.out.println("Veuillez faire votre choix");
+			user_choice = questionUser.nextInt();
+			
+			
+		
 		}
-		
-		
-		
+		System.out.println("Aurevoir :(  ");
 		
 
 	}

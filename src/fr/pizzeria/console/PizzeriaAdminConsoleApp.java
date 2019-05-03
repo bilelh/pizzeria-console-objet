5e32e71848c606c686d2d7158f2714e21b0dd856
bilel <bilelh532@gmail.com>
2019-05-03 12:16:21 +0200

    Ajout du choix par entrée clavier

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,9 +1,14 @@
 package fr.pizzeria.console;
 
+import java.util.Scanner;
+
 public class PizzeriaAdminConsoleApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner questionUser = new Scanner(System.in);
+		//int nombre_1 = questionUser.nextInt();
 		
 		System.out.println("***** Pizzeria Administration *****  ");
 		System.out.println("1. Lister les pizzas  ");
@@ -11,6 +16,22 @@ public class PizzeriaAdminConsoleApp {
 		System.out.println("3. Mettre à jour une pizza  ");
 		System.out.println("4. Supprimer une pizza  ");
 		System.out.println("99. Sortir  ");
+		
+		System.out.println("Veuillez faire votre choix");
+		int user_choice = questionUser.nextInt();
+		
+		if (user_choice == 1) {
+			System.out.println("Liste des pizzas  ");
+		} else if ( user_choice == 2) {
+			System.out.println("Ajout d'une nouvelle pizza  ");
+		} else if ( user_choice == 3) {
+			System.out.println("Mise à jour d'une pizza  ");
+		} else if ( user_choice == 4) {
+			System.out.println("Suppression d'une pizza ");
+		} else if ( user_choice == 99) {
+			System.out.println("Aurevoir :(  ");
+		}
+		
 		
 		
 		
